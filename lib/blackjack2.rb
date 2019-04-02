cc5c6087759c36dac8681b1762e80ad71a07264a
B.A. Hanlon <herbivore137@gmail.com>
2019-04-02 01:47:01 +0000

    Automatically backed up by Learn

--- lib/blackjack2.rb
+++ lib/blackjack2.rb
@@ -61,7 +61,7 @@ def runner
       display_card_total(card_total)
       hit?(card_total)
   end
-  card_total == 21 ? puts "BLACKJACK!" : end_game(card_total)
+  card_total > 21 ? end_game(card_total) : puts "BLACKJACK!"
 end
     
 runner
